558037fb7ca4d46af159e6def2d83b0612229725
jw406 <jwvic.2357@gmail.com>
2021-06-24 14:01:17 -0400

    login page init

---
+++ src/main/java/org/servlets/Login.java
@@ -0,0 +1,20 @@
+package org.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    req.getRequestDispatcher("login.jsp").forward(req, resp);
+  }
+
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  }
+}
