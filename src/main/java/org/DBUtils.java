37a33e912ae7024bbe2f23fdd97340a6579ec2f5
JW406 <jwvic.2357@gmail.com>
2023-06-30 17:36:03 -0500

    set up hibernate to RDS connection

---
+++ src/main/java/org/DBUtils.java
@@ -0,0 +1,41 @@
+package org;
+
+import java.io.File;
+import java.net.URL;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class DBUtils {
+  private static SessionFactory sessionFactory;
+  static {
+    URL url = DBUtils.class.getClassLoader().getResource("hibernate.cfg.xml");
+    File hibernateCfg = null;
+    try {
+      if (url == null) {
+        throw new Exception();
+      }
+      hibernateCfg = new File(url.toURI());
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new HibernateException("File not found");
+    }
+    final Configuration config = new Configuration().configure(hibernateCfg);
+
+    // read in RDS credentials from the environment variables.
+    config.setProperty("hibernate.connection.url", System.getenv("rds_url"));
+    config.setProperty("hibernate.connection.username", System.getenv("rds_username"));
+    config.setProperty("hibernate.connection.password", System.getenv("rds_pwd"));
+
+    final StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+        .applySettings(config.getProperties());
+    sessionFactory = config.buildSessionFactory(builder.build());
+  }
+
+  public static Session getSession() {
+    return sessionFactory.openSession();
+  }
+}
