0310496ad17c0ea03d3bede24586e0200e742bc5
JW406 <jwvic.2357@gmail.com>
2023-07-13 04:18:08 -0500

    add some javadocs

--- src/main/java/org/services/Interface/AccountServices.java
+++ src/main/java/org/services/Interface/AccountServices.java
@@ -11,18 +11,58 @@ import org.models.Employee;
 import org.models.Manager;
 import org.models.Person;
 
+/**
+ * General Account Services
+ */
 public interface AccountServices {
+
+  /**
+   * @param email
+   * @return Person
+   */
+  Person getPersonRecordByEmail(String email);
+
+  /**
+   * @param rc
+   * @return Boolean
+   */
   Boolean registerAccount(RegisterCredentials rc);
 
+  /**
+   * @param lc
+   * @return Person
+   */
   Person loginAccount(LoginCredentials lc);
 
+  /**
+   * @param lc
+   * @param email
+   * @return Boolean
+   */
   Boolean updateAccountInfo(UpdateAccountInfo lc, String email);
 
+  /**
+   * @param pcr
+   * @param email
+   * @return Boolean
+   */
   Boolean updateAccountPassword(PasswordChangeRequest pcr, String email);
 
+  /**
+   * @param email
+   * @return List<Employee>
+   */
   List<Employee> getAllEmployeesByManager(String email);
 
+  /**
+   * @return List<Manager>
+   */
   List<Manager> getAllManagers();
 
+  /**
+   * @param email
+   * @param rr
+   * @return Boolean
+   */
   Boolean employeeChangeManager(String email, EmployeeChangeManagerRequest rr);
 }
--- src/main/java/org/services/Interface/ReimbursementService.java
+++ src/main/java/org/services/Interface/ReimbursementService.java
@@ -7,16 +7,49 @@ import org.RestModels.sendReimbursementRequest;
 import org.models.ReimbursementRequest;
 import org.models.ReimbursementState;
 
+/**
+ * General Reimbursement Services
+ */
 public interface ReimbursementService {
+  /**
+   * @param rr sendReimbursementRequest
+   * @param email String
+   * @return Boolean
+   */
   Boolean employeeSendReimbursementRequest(sendReimbursementRequest rr, String email);
 
+  /**
+   * @param rr SubmitReimbursementUpdateRequest
+   * @param email String
+   * @return Boolean
+   */
   Boolean updateReimbursementRequest(SubmitReimbursementUpdateRequest rr, String email);
 
+  /**
+   * @param email String
+   * @param state ReimbursementState
+   * @return List<ReimbursementRequest>
+   */
   List<ReimbursementRequest> getReimbursementRequestsByLoggedInEmail(String email, ReimbursementState state);
 
+  /**
+   * @param email
+   * @param state
+   * @return List<ReimbursementRequest>
+   */
   List<ReimbursementRequest> getManagedEmployeeRequests(String email, ReimbursementState state);
 
+  /**
+   * @param rr
+   * @param email
+   * @return Boolean
+   */
   Boolean managerAcceptReimbursementRequest(SubmitReimbursementUpdateRequest rr, String email);
 
+  /**
+   * @param rr
+   * @param email
+   * @return Boolean
+   */
   Boolean managerReimbursementRequestComment(SubmitReimbursementUpdateRequest rr, String email);
 }
